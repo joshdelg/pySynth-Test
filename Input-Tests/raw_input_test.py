c081db4f473af5b81d46d3d0dcd92d4c27cbba68
joshdelg <delgadillo.i.joshua@gmail.com>
2017-07-20 16:51:08 -0500

    Finished raw_input.py

--- Input-Tests/raw_input_test.py
+++ Input-Tests/raw_input_test.py
@@ -1,1 +1,18 @@
 import pysynth
+
+# Ex. > e 4 d 4 c 4 d 4 e 4 e 4 e 2
+notes = raw_input('Enter notes: ').split()
+
+song = []
+
+# after playing around, decided a while lope would be better for incrementing
+i = 0
+
+while i < len(notes) - 1:
+  
+	temp = (notes[i], int(notes[i + 1]))
+	song.append(temp)
+  
+	i += 2
+
+pysynth.make_wav(song, fn = 'raw_input.wav')
